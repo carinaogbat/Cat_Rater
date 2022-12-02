f23b2b7dd162a6583d938ed7fe68d1edc6a39f4e
Carina Ogbat <carinawrightogbat@gmail.com>
2022-12-01 20:30:53 -0800

    added images

---
+++ crud.py
@@ -0,0 +1,65 @@
+from model import db, User, Photo, Rating, connect_to_db
+
+
+def create_user(username, email, password):
+    """Creates a User"""
+
+    user = User(username=username, email=email, 
+    password=password)
+
+    return user
+
+def create_photo(url, text, username):
+    """Creates a photo"""
+
+    photo = Photo(url=url, text=text, username=username)
+
+    return photo
+
+def create_rating(user, photo, score):
+    """Scores a photo"""
+
+    rating = Rating(user=user, photo=photo, score=score)
+
+    return rating
+
+def get_user_by_id(user_id):
+    """Returns a user by ID"""
+
+    return User.query.get(user_id)
+
+def get_user_by_username(username):
+    """Returns a user by Username"""
+
+    return User.query.filter(User.username == username).first()
+
+def get_user_by_email(email):
+    """Returns user by email"""
+
+    return User.query.filter(User.email == email).first()
+
+def get_all_users():
+    """Return all users"""
+
+    return User.query.all()
+
+def get_all_photos():
+    """Returns all photos"""
+
+    return Photo.query.all()
+
+def get_users_photos(username):
+    """Returns user photos"""
+
+    return Photo.query.filter(Photo.username == username)
+
+
+def get_photo_by_id(photo_id):
+    """Gets a photo by ID"""
+
+    return Photo.query.get(photo_id)
+
+# if __name__ == '__main__':
+#     from server import app
+#     connect_to_db(app)
+
---
+++ seed_database.py
@@ -0,0 +1,84 @@
+import os
+import json
+import crud
+import model
+import server
+from random import choice, randint
+
+os.system("dropdb cat_rater")
+os.system("createdb cat_rater")
+
+model.connect_to_db(server.app)
+model.db.create_all()
+
+with open("data/cats.json") as f:
+    data = json.loads(f.read())
+
+users_in_db = []
+for user in data:
+    username, email, password = (
+        user["username"], user["email"], user["password"]
+    )
+    db_user = crud.create_user(username,
+        email, password)
+    users_in_db.append(db_user)
+
+
+model.db.session.add_all(users_in_db)
+model.db.session.commit()
+
+photos_in_db = []
+for photo in data:
+    url, text, username = (
+        photo["profile_img"], photo["bio"], photo["username"]
+    )
+    db_photo = crud.create_photo(url, text, username)
+    photos_in_db.append(db_photo)
+
+model.db.session.add_all(photos_in_db)
+model.db.session.commit()
+
+users = crud.get_all_users()
+for user in users:
+    random_cat = choice(photos_in_db)
+    score = randint(1, 5)
+
+    rating = crud.create_rating(user=user, photo=random_cat, score=score)
+    model.db.session.add(rating)
+
+model.db.session.commit()
+
+
+
+# ogbot_photos_in_db = ["/static/imgs/TiniestFloof.jpeg", "static/imgs/MouseFloof.jpeg", 
+#     "static/imgs/YoungFloof.jpeg", "static/imgs/NappingFloof.jpeg", "static/imgs/ComputerFloof.jpeg"]
+# jamie_photos_in_db = ["static/imgs/Pepperoni.jpg", "static/imgs/Chloe.jpg", "static/imgs/BlackJack.jpg"]
+# lois_photos_in_db = ["static/imgs/Dixie.jpg", "static/imgs/Athena.jpg"]
+# chelsea_photos_in_db = ["static/imgs/Mac2ndPhoto.jpg", "static/imgs/Charlie.jpg", "static/imgs/MacAndCharlie.jpg"]
+
+# ogbot = crud.get_user_by_username("OgBot")
+# db_photos = []
+# for photo_url in ogbot_photos_in_db:
+#     db_photo = crud.create_photo(username=ogbot.username, url=photo_url)
+#     db_photos.append(db_photo)
+
+# jamie = crud.get_user_by_username("jamie")
+# for photo_url in jamie_photos_in_db:
+#     db_photo = crud.create_photo(username=jamie.username, url=photo_url)
+#     db_photos.append(db_photo)
+
+# lois = crud.get_user_by_username("lois")
+# for photo_url in lois_photos_in_db:
+#     db_photo = crud.create_photo(lois=lois.username, url=photo_url)
+#     db_photos.append(db_photo)
+
+# chelsea = crud.get_user_by_username("chelsea")
+# for photo_url in chelsea_photos_in_db:
+#     db_photo = crud.create_photo(chelsea=chelsea.username, url=photo_url)
+#     db_photos.append(db_photo)
+
+
+# model.db.session.add_all(db_photos)
+
+
+model.db.session.commit()
