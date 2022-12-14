90284a60dfdfff98eeb8b6f4839a07f14ca8edbb
Carina Ogbat <carinawrightogbat@gmail.com>
2022-12-13 20:49:38 -0800

    forgot to add js file to git

---
+++ cat_rater.js
@@ -0,0 +1,21 @@
+document.querySelector('#photo-upload').addEventListener('submit', (evt)=>
+{
+    evt.preventDefault();
+    const fileInput = {
+        file : document.querySelector('#file').value,
+        text : document.querySelector('#photo-text').value,
+        name : document.querySelector('#photo-name').value
+    };
+    fetch("/myprofile/<username>", {
+        method: 'POST',
+        body: JSON.stringify(fileInput),
+        headers: {
+            'Content-Type': 'application/json',
+        },
+    })
+    .then((response) => response.json())
+    .then((responseJson) => {
+        alert(responseJson.status);
+    });
+});
+
